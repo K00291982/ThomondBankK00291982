eeda272ae00756adfa6591047a35e15f2508da90
Mateusz <K00291982@student.tus.ie>
2025-04-06 22:06:47 +0100

    Added accounts list to ATMGUI

--- src/ATMGUI.java
+++ src/ATMGUI.java
@@ -37,6 +37,19 @@ public class ATMGUI {
         frame.setVisible(true);
     }
 
-
+    private void populateMyAccounts() {
+// used to populate myAccounts ArrayList with test data
+        thomondAccounts.add(new DepositAccount(1, 1));
+        thomondAccounts.get(0).deposit(100);
+        thomondAccounts.add(new DepositAccount(2, 2));
+        thomondAccounts.get(1).deposit(500);
+        thomondAccounts.add(new DepositAccount(3, 3));
+        thomondAccounts.get(2).deposit(300);
+        thomondAccounts.add(new DepositAccount(4, 4));
+        thomondAccounts.get(3).deposit(300);
+        thomondAccounts accounts.add(new CurrentAccount(4, 1, 100));
+        thomondAccounts.add(new CurrentAccount(5, 2, 1000));
+        thomondAccounts.add(new CurrentAccount(6, , 4, 200));
+    }
 
 }
